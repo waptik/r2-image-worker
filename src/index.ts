620afbadd083df532ca733567fb98fe9b6204b71
TheVirginBrokey <itswaptik@gmail.com>
2024-08-12 11:31:25 +0000

    upload images from parsable url

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,75 @@
-import { Hono } from 'hono/quick'
-import { cache } from 'hono/cache'
-import { sha256 } from 'hono/utils/crypto'
-import { basicAuth } from 'hono/basic-auth'
-import { detectType } from './utils'
+import { Hono } from "hono/quick";
+import { cache } from "hono/cache";
+import { basicAuth } from "hono/basic-auth";
+import { detectType } from "./utils";
 
 type Bindings = {
-  BUCKET: R2Bucket
-  USER: string
-  PASS: string
-}
+  BUCKET: R2Bucket;
+  USER: string;
+  PASS: string;
+};
 
 type Data = {
-  body: string
-  width?: string
-  height?: string
-}
+  body: string;
+  width?: string;
+  height?: string;
+};
 
-const maxAge = 60 * 60 * 24 * 30
+const maxAge = 60 * 60 * 24 * 365 * 2;
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.put('/upload', async (c, next) => {
-  const auth = basicAuth({ username: c.env.USER, password: c.env.PASS })
-  await auth(c, next)
-})
+app.put("/upload", async (c, next) => {
+  const auth = basicAuth({ username: c.env.USER, password: c.env.PASS });
+  await auth(c, next);
+});
 
-app.put('/upload', async (c) => {
-  const data = await c.req.json<Data>()
-  const base64 = data.body
-  if (!base64) return c.notFound()
+app.put("/upload", async (c) => {
+  const data = await c.req.json<Data>();
+  const url = data.body;
 
-  const type = detectType(base64)
-  if (!type) return c.notFound()
-
-  const body = Uint8Array.from(atob(base64), (c) => c.charCodeAt(0))
-
-  let key
-
-  if (data.width && data.height) {
-    key = (await sha256(body)) + `_${data.width}x${data.height}` + '.' + type?.suffix
-  } else {
-    key = (await sha256(body)) + '.' + type?.suffix
+  try {
+    new URL(url);
+  } catch (error) {
+    return c.notFound();
   }
 
-  await c.env.BUCKET.put(key, body, { httpMetadata: { contentType: type.mimeType } })
+  const res = await fetch(url);
+  if (!res.ok) return c.notFound();
+  const arrayBuffer = await res.arrayBuffer();
+  const b64 = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
 
-  return c.text(key)
-})
+  const type = detectType(b64);
+  if (!type) return c.notFound();
+
+  const key = url + "." + type.suffix;
+
+  await c.env.BUCKET.put(key, arrayBuffer, {
+    httpMetadata: { contentType: type.mimeType },
+  });
+
+  return c.text(key);
+});
 
 app.get(
-  '*',
+  "*",
   cache({
-    cacheName: 'r2-image-worker'
+    cacheName: "r2-image-worker",
   })
-)
+);
 
-app.get('/:key', async (c) => {
-  const key = c.req.param('key')
+app.get("/:key", async (c) => {
+  const key = c.req.param("key");
 
-  const object = await c.env.BUCKET.get(key)
-  if (!object) return c.notFound()
-  const data = await object.arrayBuffer()
-  const contentType = object.httpMetadata?.contentType ?? ''
+  const object = await c.env.BUCKET.get(key);
+  if (!object) return c.notFound();
+  const data = await object.arrayBuffer();
+  const contentType = object.httpMetadata?.contentType ?? "";
 
   return c.body(data, 200, {
-    'Cache-Control': `public, max-age=${maxAge}`,
-    'Content-Type': contentType
-  })
-})
+    "Cache-Control": `public, max-age=${maxAge}`,
+    "Content-Type": contentType,
+  });
+});
 
-export default app
+export default app;
